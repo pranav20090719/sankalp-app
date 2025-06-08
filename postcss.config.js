befd500f5c8c18692f3d93d7df6cd9ecf82cbcf1
pranav20090719 <sanatanam.jaishriram@gmail.com>
2025-06-09 00:37:07 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 export default {
-  plugins: {
-    tailwindcss: {}, // Tailwind CSS plugin
-    autoprefixer: {}, // Autoprefixer adds vendor prefixes to CSS rules
-  },
+          plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+          },
 }
